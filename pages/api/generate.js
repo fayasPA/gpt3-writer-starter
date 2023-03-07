3df55ac55b6527812639d873d1330832a353e65d
fayasPA <fayasmuthalib@gmail.com>
2023-03-07 16:20:45 +0530

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,29 @@
+import { Configuration, OpenAIApi,  } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `
+write me a cover letter for IT companies on the details given below and also highlight the company name and position applying:
+
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
